61afd545368391e86d4fc0e752e2a3d23bd98d78
Jeffrey H. Johnson <trnsz@pobox.com>
2022-07-23 19:46:09 -0400

    Appease audit

--- reduce.rb
+++ reduce.rb
@@ -10,7 +10,7 @@ class Reduce < Formula
 
   # Supports REDUCE builds from stable release snapshots or Subversion HEAD.
 
-  # TODO: (items below)
+  # What still needs to be completed before cleaning up for homebrew-core?
   # * REDUCE doesn't like Homebrew gnuplot ...
   # - - gnuplot> if(strstrt(GPVAL_TERMINALS,"aqua")!=0)set terminal aqua;;    set term x11;
   # - - line 0: unknown or ambiguous terminal type; type just 'set terminal' for a list
